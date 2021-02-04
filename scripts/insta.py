6175960c6debf1e0d8ff9a05d6f6678f2868aac4
Avy Faingezicht <avyfain@gmail.com>
2021-02-03 21:20:58 -0800

    add film I

--- scripts/insta.py
+++ scripts/insta.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 from pathlib import Path
 from PIL import Image, ImageOps
@@ -11,17 +12,21 @@ def parse_args():
 def get_border_size(img):
     a = [max(img.size)]*2
     b = img.size
-    return tuple((x - y) // 2 for x, y in zip(a, b))
+    square_border = tuple((x - y) // 2 for x, y in zip(a, b))
+    padding = tuple(int(sum(x)) for x in zip([max(img.size) * .05]*2, square_border))
+    return padding
 
 def main():
     args = parse_args()
-	target_dir = Path(args.out_dir)
+    input_dir = Path(args.input_dir)
+    target_dir = Path(args.out_dir)
+    target_dir.mkdir(parents=True, exist_ok=True)
 
-	for idx, i in enumerate(os.listdir(args.input_dir)):
-	    img = Image.open(i)
-	    border = get_border_size(img)
-	    img = ImageOps.expand(img, border=border, fill="white")
-	    img.save(target_dir / Path(f'{idx +1}.jpeg'))
+    for idx, i in enumerate(os.listdir(input_dir)):
+        img = Image.open(input_dir / Path(i))
+        border = get_border_size(img)
+        img = ImageOps.expand(img, border=border, fill="white")
+        img.save(target_dir / Path(f'{idx +1}.jpeg'))
 
 
 if __name__ == '__main__':
--- scripts/resize_photos.py
+++ scripts/resize_photos.py
@@ -9,6 +9,7 @@ logger = logging.getLogger(__name__)
 
 
 class Thumbnailer(object):
+    preview_dirname = 'previews'
     SIZES = [(".l", 3000),
              (".m", 2000),
              ("", 1280),
@@ -16,16 +17,20 @@ class Thumbnailer(object):
              (".xs", 600),
              ]
 
-    def __init__(self, directory):
+    def __init__(self, directory, books=False):
         self.directory = directory
-        self.preview_dirpath = os.path.join(directory, 'previews')
+        if books:
+            self.SIZES = [('', 500)]
+            self.preview_dirname = 'books'
+
+        self.preview_dirpath = os.path.join(directory, self.preview_dirname)
 
         if not os.path.exists(self.preview_dirpath):
             os.makedirs(self.preview_dirpath)
 
     def process_files(self, files):
         for f in files:
-            if not f.endswith(('jpeg', 'jpg', 'png')):
+            if not f.endswith(('jpeg', 'jpg', 'png', 'webp')):
                 continue
             self.create_thumbnails(f)
 
@@ -33,9 +38,10 @@ class Thumbnailer(object):
         filepath = os.path.join(self.directory, f)
         logger.info("Processing %s", f)
         with Image.open(filepath) as im:
+            im = im.convert('RGB')
             for suffix, size in self.SIZES:
                 filename, ext = os.path.splitext(f)
-                new_name = filename + suffix + ext
+                new_name = filename + suffix + '.jpeg'
                 logger.info("Image %s, size %s", f, size)
                 previewpath = os.path.join(self.preview_dirpath, new_name)
                 if os.path.exists(previewpath):
@@ -47,6 +53,7 @@ class Thumbnailer(object):
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input_dir', action='store', dest='input_dir', required=True)
+    parser.add_argument('-b', '--books', action='store_true')
     return parser.parse_args()
 
 
@@ -54,7 +61,7 @@ def main():
     args = parse_args()
 
     dirpath, dirnames, filenames = next(os.walk(args.input_dir))
-    t = Thumbnailer(args.input_dir)
+    t = Thumbnailer(args.input_dir, args.books)
     t.process_files(filenames)
 
 
